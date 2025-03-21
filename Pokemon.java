7fe6076c45896f41f66c3cc8273a58565aed0282
rhit-heddenaj <143573330+rhit-heddenaj@users.noreply.github.com>
2025-03-21 15:16:44 -0400

    TestSetup passes

--- Pokemon.java
+++ Pokemon.java
@@ -1,5 +1,19 @@
 public class Pokemon {
-    public static void main(String[] args) {
-        System.out.println("This will eventually be a Pokemon game!");
+
+    String name;
+    String type;
+    int stage;
+    int hp;
+    public Pokemon(String name, String type, int stage, int hp) {
+        this.name = name;
+        this.type = type;
+        this.stage = stage;
+        this.hp = hp;
     }
+
+    public void takeDamage(int damage) {
+        hp -= damage;
+    }
+
+
 }
--- PokemonTest.java
+++ PokemonTest.java
@@ -1,5 +1,23 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class PokemonTest {
-    public static void main(String[] args) {
-        Pokemon p = new Pokemon();
+
+    @Test
+    public void testSetup() {
+        Pokemon p = new Pokemon("Pikachu", "Fire", 1, 40);
+        assertEquals("Pikachu", p.name);
+        assertEquals("Fire", p.type);
+        assertEquals(1, p.stage);
+        assertEquals(40, p.hp);
     }
+
+    @Test
+    public void testTakeDamage() {
+        Pokemon p = new Pokemon("Pikachu", "Fire", 1, 40);
+        p.takeDamage(10);
+        assertEquals(30, p.hp);
+    }
+
 }
