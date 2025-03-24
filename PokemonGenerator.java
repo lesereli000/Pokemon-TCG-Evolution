c6281b3df1cceafd2b9fa1612d2c62a767de7ae2
rhit-heddenaj <143573330+rhit-heddenaj@users.noreply.github.com>
2025-03-23 21:39:16 -0400

    testManyPokemon now works, actually getting data from base1.json!

--- PokemonGenerator.java
+++ PokemonGenerator.java
@@ -1,3 +1,10 @@
+import java.io.FileNotFoundException;
+import org.json.*;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class PokemonGenerator {
 
     String name;
@@ -5,15 +12,42 @@ public class PokemonGenerator {
     int hp;
     int stage;
 
+    // When given the name of a pokemon, should be able to create a Pokemon object with all desired information
+
     public PokemonGenerator(String name) {
         this.name = name;
-        this.type = "Fire";
-        this.hp = 120;
-        this.stage = 2;
+        try (FileReader reader = new FileReader("base1.json")) {
+            String content = new String(Files.readAllBytes(Paths.get("base1.json")));
+//            System.out.println(content);
+            JSONArray pokemonArray = new JSONArray(content);
+
+            for (int i = 0; i < pokemonArray.length(); i++) {
+
+                // Found JSON examples on https://www.tutorialspoint.com/json/json_java_example.htm
+
+                // Further referenced https://www.geeksforgeeks.org/working-with-json-data-in-java/
+
+                if(pokemonArray.getJSONObject(i).getString("name").equals(name)) {
+                      this.type = pokemonArray.getJSONObject(i).getJSONArray("types").getString(0);
+                      this.hp = pokemonArray.getJSONObject(i).getInt("hp");
+                      String wholeStage = pokemonArray.getJSONObject(i).getJSONArray("subtypes").getString(0);
+                      if (wholeStage.equals("Basic")) {
+                          this.stage = 0;
+                      } else {
+                          this.stage = Integer.parseInt(wholeStage.substring(wholeStage.length()-1));
+                      }
+                      break;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("File not found");
+            return;
+        }
     }
 
     public Pokemon generate() {
-        return new Pokemon(name, type, stage, hp);
+        return new Pokemon(name, type,stage, hp);
     }
 
 }
--- PokemonTest.java
+++ PokemonTest.java
@@ -11,16 +11,16 @@ public class PokemonTest {
 
     @Test
     public void testSetup() {
-        Pokemon p = new Pokemon("Pikachu", "Fire", 1, 40);
+        Pokemon p = new Pokemon("Pikachu", "Lightning", 1, 40);
         assertEquals("Pikachu", p.name);
-        assertEquals("Fire", p.type);
+        assertEquals("Lightning", p.type);
         assertEquals(1, p.stage);
         assertEquals(40, p.hp);
     }
 
     @Test
     public void testDamage() {
-        Pokemon p = new Pokemon("Pikachu", "Fire", 1, 40);
+        Pokemon p = new Pokemon("Pikachu", "Lightning", 1, 40);
         p.takeDamage(10);
         assertEquals(30, p.hp);
     }
@@ -34,7 +34,7 @@ public class PokemonTest {
     @Test
     public void testOneCard() {
         Deck d = new Deck();
-        Pokemon p = new Pokemon("Pikachu", "Fire", 1, 40);
+        Pokemon p = new Pokemon("Pikachu", "Lightning", 1, 40);
         d.addCard(p);
         assertEquals(1, d.size());
     }
@@ -42,7 +42,7 @@ public class PokemonTest {
     @Test
     public void testFirstCard() {
         Deck d = new Deck();
-        Pokemon p = new Pokemon("Pikachu", "Fire", 1, 40);
+        Pokemon p = new Pokemon("Pikachu", "Lightning", 1, 40);
         d.addCard(p);
         ArrayList<Pokemon> pokemons = d.getCards();
         assertEquals(pokemons.get(0), p);
@@ -50,13 +50,50 @@ public class PokemonTest {
 
     @Test
     public void testPokemonGen() {
-        PokemonGenerator gen = new PokemonGenerator("Charizard");
-        Pokemon p = gen.generate();
-        Pokemon charizard = new Pokemon("Charizard", "Fire", 2, 120);
-        assertEquals(charizard.name, p.name);
-        assertEquals(charizard.type, p.type);
-        assertEquals(charizard.hp, p.hp);
-        assertEquals(charizard.stage, p.stage);
+        Pokemon p = new PokemonGenerator("Charizard").generate();
+
+        assertEquals("Charizard", p.name);
+        assertEquals("Fire", p.type);
+        assertEquals(120, p.hp);
+        assertEquals(2, p.stage);
+    }
+
+    @Test
+    public void testManyPokemon() {
+        Pokemon p1 = new PokemonGenerator("Charizard").generate();
+        Pokemon p2 = new PokemonGenerator("Pikachu").generate();
+        Pokemon p3 = new PokemonGenerator("Squirtle").generate();
+        Pokemon p4 = new PokemonGenerator("Alakazam").generate();
+        Pokemon p5 = new PokemonGenerator("Arcanine").generate();
+
+        assertEquals("Charizard", p1.name);
+        assertEquals("Fire", p1.type);
+        assertEquals(2, p1.stage);
+        assertEquals(120, p1.hp);
+
+        assertEquals("Pikachu", p2.name);
+        assertEquals("Lightning", p2.type);
+        assertEquals(0, p2.stage);
+        assertEquals(40, p2.hp);
+
+        assertEquals("Squirtle", p3.name);
+        assertEquals("Water", p3.type);
+        assertEquals(0, p3.stage);
+        assertEquals(40, p3.hp);
+
+        assertEquals("Alakazam", p4.name);
+        assertEquals("Psychic", p4.type);
+        assertEquals(2, p4.stage);
+        assertEquals(80, p4.hp);
+
+        assertEquals("Arcanine", p5.name);
+        assertEquals("Fire", p5.type);
+        assertEquals(1, p5.stage);
+        assertEquals(100, p5.hp);
+
+
+
+
     }
 
     @Test
