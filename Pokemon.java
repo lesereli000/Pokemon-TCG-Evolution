111cbf921dbac117f829a3cc5f4c15a3df4507cc
Eli Leser <ejleser@gmail.com>
2025-03-27 13:23:00 -0400

    fix compile issues

--- Pokemon.java
+++ Pokemon.java
@@ -44,4 +44,34 @@ public class Pokemon extends Card{
         this.weakness = weakness;
         this.resistance = resistance;
     }
+
+    public Pokemon(String name, String type, int stage, int hp) {
+        this(name, type, stage, hp, 'Z', 'Z');
+    }
+
+    public String getName() {
+        return super.getName();
+    }
+
+    public void takeDamage(int damageCountersTaken, char damageType) {
+        if(damageType == resistance) {
+            damageCountersTaken--;
+        } else if (damageType == weakness) {
+            damageCountersTaken = damageCountersTaken * 2;
+        }
+        damageCounters += damageCountersTaken;
+    }
+
+    public int getMaxHP(){
+        return hp;
+    }
+
+    public int getDamageCounters(){
+        return damageCounters;
+    }
+
+    public int getCurHP(){
+        return hp - (10 * damageCounters);
+    }
+
 }
