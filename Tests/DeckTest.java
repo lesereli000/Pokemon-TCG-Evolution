4536555567137134e0cc4b89ec1ec8aab54d7a36
rhit-heddenaj <143573330+rhit-heddenaj@users.noreply.github.com>
2025-03-29 15:43:10 -0400

    DeckTest refactored and testHowManyRepeats passes

--- Tests/DeckTest.java
+++ Tests/DeckTest.java
@@ -26,7 +26,7 @@ public class DeckTest {
     @Test
     public void testOneCard() {
         Deck d = new Deck();
-        Pokemon p = createMock(Pokemon.class);
+        Card p = createMock(Pokemon.class);
         d.addCard(p);
         assertEquals(1, d.size());
     }
@@ -34,7 +34,7 @@ public class DeckTest {
     @Test
     public void testGetLargeSizeDeck() {
         Deck d = new Deck();
-        Energy e = createMock(Energy.class);
+        Card e = createMock(Energy.class);
         for (int i = 0; i < 300; i++) {
             d.addCard(e);
         }
@@ -44,10 +44,190 @@ public class DeckTest {
     @Test
     public void testFirstCard() {
         Deck d = new Deck();
-        Pokemon p = createMock(Pokemon.class);
+        Card p = createMock(Pokemon.class);
         d.addCard(p);
         ArrayList<Card> pokemons = d.getCards();
         assertEquals(pokemons.get(0), p);
+    }
+
+    @Test
+    public void testAddTrainerToDeck() {
+        Card t = createMock(Trainer.class);
+        expect(t.getName()).andReturn("Scoop Up");
+        replay(t);
+        Deck d = new Deck();
+
+        d.addCard(t);
+        assertEquals(1, d.size());
+        assertEquals(t, d.getCards().get(0));
+        assertEquals("Scoop Up", (d.getCards().get(0)).getName());
+        verify(t);
+    }
+
+    @Test
+    public void mixTrainersAndPokemon() {
+        Card trainer = createMock(Trainer.class);
+        expect(trainer.getName()).andReturn("Scoop Up").anyTimes();
+        replay(trainer);
+
+        Card p1 = createMock(Pokemon.class);
+        expect(p1.getName()).andReturn("Charizard").anyTimes();
+        replay(p1);
+
+        Card p2 = createMock(Pokemon.class);
+        expect(p2.getName()).andReturn("Pikachu").anyTimes();
+        replay(p2);
+
+        Card p3 = createMock(Pokemon.class);
+        expect(p3.getName()).andReturn("Squirtle").anyTimes();
+        replay(p3);
+
+        Deck d = new Deck();
+
+        d.addCard(p1);
+        d.addCard(p2);
+        d.addCard(trainer);
+        d.addCard(p3);
+
+        assertEquals(4, d.size());
+        assertEquals(p1.getName(), d.getCards().get(0).getName());
+        assertEquals(p2.getName(), d.getCards().get(1).getName());
+        assertEquals(trainer.getName(), d.getCards().get(2).getName());
+        assertEquals(p3.getName(), d.getCards().get(3).getName());
+
+        verify(trainer);
+        verify(p1);
+        verify(p2);
+        verify(p3);
+    }
+
+    @Test
+    public void testAddEnergyToDeck() {
+        Card e = createMock(Energy.class);
+        expect(e.getName()).andReturn("Lightning Energy").anyTimes();
+        replay(e);
+        Deck d = new Deck();
+        d.addCard(e);
+        assertEquals(1, d.size());
+        assertEquals(e, d.getCards().get(0));
+        assertEquals("Lightning Energy", (d.getCards().get(0)).getName());
+        verify(e);
+    }
+
+    @Test
+    public void testMakeFullDeck() {
+        Card trainer = createMock(Trainer.class);
+        expect(trainer.getName()).andReturn("Scoop Up").anyTimes();
+        replay(trainer);
+
+        Card e = createMock(Energy.class);
+        expect(e.getName()).andReturn("Lightning Energy").anyTimes();
+        replay(e);
+
+        Card e2 = createMock(Energy.class);
+        expect(e2.getName()).andReturn("Psychic Energy").anyTimes();
+        replay(e2);
+
+        Deck d = new Deck();
+
+        Card p1 = createMock(Pokemon.class);
+        expect(p1.getName()).andReturn("Charizard").anyTimes();
+        replay(p1);
+
+        Card p2 = createMock(Pokemon.class);
+        expect(p2.getName()).andReturn("Pikachu").anyTimes();
+        replay(p2);
+
+        Card p3 = createMock(Pokemon.class);
+        expect(p3.getName()).andReturn("Squirtle").anyTimes();
+        replay(p3);
+
+        d.addCard(p1);
+        d.addCard(e);
+        d.addCard(p2);
+        d.addCard(trainer);
+        d.addCard(p3);
+        d.addCard(e2);
+
+        assertEquals(6, d.size());
+        assertEquals(p1, d.getCards().get(0));
+        assertEquals(e, d.getCards().get(1));
+        assertEquals(p2, d.getCards().get(2));
+        assertEquals(trainer, d.getCards().get(3));
+        assertEquals(p3, d.getCards().get(4));
+        assertEquals(e2, d.getCards().get(5));
+
+        verify(trainer);
+        verify(e);
+        verify(e2);
+        verify(p1);
+        verify(p2);
+        verify(p3);
+    }
+
+    @Test
+    public void testHowManyRepeatsZero() {
+        Deck d = new Deck();
+        Card p = createMock(Pokemon.class);
+        expect(p.getName()).andReturn("Pikachu").anyTimes();
+        replay(p);
+
+        assertEquals(0, d.howManyRepeats(p));
+
+        verify(p);
+    }
+
+    @Test
+    public void testHowManyRepeatsOne() {
+        Deck d = new Deck();
+
+        Card p = createMock(Pokemon.class);
+        expect(p.getName()).andReturn("Pikachu").anyTimes();
+        replay(p);
+
+        d.addCard(p);
+
+        assertEquals(1, d.howManyRepeats(p));
+
+        verify(p);
+    }
+
+    @Test
+    public void testHowManyRepeatsMany() {
+        Deck d = new Deck();
+
+        Card p = createMock(Pokemon.class);
+        expect(p.getName()).andReturn("Pikachu").anyTimes();
+        replay(p);
+
+        Card p2 = createMock(Pokemon.class);
+        expect(p2.getName()).andReturn("Squirtle").anyTimes();
+        replay(p2);
+
+        Card e = createMock(Energy.class);
+        expect(e.getName()).andReturn("Lightning Energy").anyTimes();
+        replay(e);
+
+        d.addCard(p);
+        d.addCard(p2);
+        d.addCard(e);
+
+        assertEquals(1, d.howManyRepeats(p));
+        assertEquals(1, d.howManyRepeats(p2));
+        assertEquals(1, d.howManyRepeats(e));
+
+        d.addCard(p);
+        d.addCard(e);
+        d.addCard(e);
+        d.addCard(e);
+
+        assertEquals(2, d.howManyRepeats(p));
+        assertEquals(1, d.howManyRepeats(p2));
+        assertEquals(4, d.howManyRepeats(e));
+
+        verify(p);
+        verify(p2);
+        verify(e);
     }
 
     // https://stackoverflow.com/questions/34233447/is-there-any-difference-between-andreturn-anytimes-and-andstubreturn
@@ -145,6 +325,28 @@ public class DeckTest {
     }
 
     @Test
+    public void testMakeRandomCards() {
+        Deck d = new Deck();
+        Random rand = createMock(Random.class);
+        expect(rand.nextInt(anyInt())).andReturn(100).anyTimes();
+        replay(rand);
+        d.addRandomCards(10, rand);
+        assertEquals(10, d.size());
+        verify(rand);
+    }
+
+    @Test
+    public void testMakeFullDeckCards() {
+        Deck d = new Deck();
+        Random rand = createMock(Random.class);
+        expect(rand.nextInt(anyInt())).andReturn(100).anyTimes();
+        replay(rand);
+        d.addRandomCards(60, rand);
+        assertEquals(60, d.size());
+        verify(rand);
+    }
+
+    @Test
     public void testNotTooManyRepeatsRandom() {
         Random rand = createMock(Random.class);
         expect(rand.nextInt(anyInt())).andReturn(1).anyTimes();
@@ -163,59 +365,6 @@ public class DeckTest {
         assertTrue(pass);
         verify(rand);
     }
-
-
-    public ArrayList<Card> getAllCards() {
-        ArrayList<Card> allCards = new ArrayList<Card>();
-        CardGenerator cg = new CardGenerator();
-
-        try (FileReader reader = new FileReader("base1.json")) {
-            String content = new String(Files.readAllBytes(Paths.get("base1.json")));
-            JSONArray pokemonArray = new JSONArray(content);
-            for (int i = 0; i < pokemonArray.length(); i++) {
-                String PokemonName = pokemonArray.getJSONObject(i).getString("name");
-                Card card = cg.generateCard(PokemonName);
-                allCards.add(card);
-            }
-        } catch (IOException e) {
-            System.out.println("File not found in getAllCards" + e);
-            return null;
-        }
-        return allCards;
-    }
-    /*
-
-    @Test
-    public void testGetPokemon() {
-        // TODO: This method appears to be testing code written to set up tests rather than production code. We have enough other tests of Pokemon Generator
-        ArrayList<Card> allCards = getAllCards();
-        //allPokemon should be in same order of base1.json file
-
-        Pokemon p1 = (Pokemon) allCards.get(0);
-        assertEquals("Alakazam", p1.getName());
-        assertEquals("Psychic", p1.type);
-        assertEquals(2, p1.stage);
-        assertEquals(80, p1.hp);
-
-        Pokemon p2 = (Pokemon) allCards.get(1);
-        assertEquals("Blastoise", p2.getName());
-        assertEquals("Water", p2.type);
-        assertEquals(2, p2.stage);
-        assertEquals(100, p2.hp);
-
-        Pokemon p3 = (Pokemon) allCards.get(45);
-        assertEquals("Charmander", p3.getName());
-        assertEquals("Fire", p3.type);
-        assertEquals(50, p3.hp);
-        assertEquals(0, p3.stage);
-
-        Pokemon p4 = (Pokemon) allCards.get(68);
-        assertEquals("Weedle", p4.getName());
-        assertEquals("Grass", p4.type);
-        assertEquals(0, p4.stage);
-        assertEquals(40, p4.hp);
-    }
-     */
 
     @Test
     public void testGetPokemonFromDeck() {
@@ -242,85 +391,23 @@ public class DeckTest {
         }
     }
 
-    @Test
-    public void testMakeRandomCards() {
-        Deck d = new Deck();
-        d.addRandomCards(10, new Random());
-        assertEquals(10, d.size());
-    }
+    public ArrayList<Card> getAllCards() {
+        ArrayList<Card> allCards = new ArrayList<Card>();
+        CardGenerator cg = new CardGenerator();
 
-    @Test
-    public void testMakeFullDeckCards() {
-        Deck d = new Deck();
-        d.addRandomCards(60, new Random());
-        assertEquals(60, d.size());
-    }
-
-    @Test
-    public void testAddTrainerToDeck() {
-        Card t = new Trainer("Scoop Up", "Test");
-        Deck d = new Deck();
-        d.addCard(t);
-        assertEquals(1, d.size());
-        assertEquals(t, d.getCards().get(0));
-        assertEquals("Scoop Up", ((Trainer) d.getCards().get(0)).getName());
-    }
-
-    @Test
-    public void mixTrainersAndPokemon() {
-        Card trainer = new Trainer("Defender", "Test");
-        Deck d = new Deck();
-        CardGenerator pg = new CardGenerator();
-        Pokemon p1 = (Pokemon) pg.generateCard("Charizard");
-        Pokemon p2 = (Pokemon) pg.generateCard("Pikachu");
-        Pokemon p3 = (Pokemon) pg.generateCard("Squirtle");
-
-        d.addCard(p1);
-        d.addCard(p2);
-        d.addCard(trainer);
-        d.addCard(p3);
-
-        assertEquals(4, d.size());
-        assertEquals(p1, d.getCards().get(0));
-        assertEquals(p2, d.getCards().get(1));
-        assertEquals(trainer, d.getCards().get(2));
-        assertEquals(p3, d.getCards().get(3));
-    }
-
-    @Test
-    public void testAddEnergyToDeck() {
-        Card e = new Energy("Lightning Energy");
-        Deck d = new Deck();
-        d.addCard(e);
-        assertEquals(1, d.size());
-        assertEquals(e, d.getCards().get(0));
-        assertEquals("Lightning Energy", ((Energy) d.getCards().get(0)).getName());
-    }
-
-    @Test
-    public void testMakeFullDeck() {
-        Card trainer = new Trainer("Scoop Up", "Test");
-        Card e = new Energy("Lightning Energy");
-        Card e2 = new Energy("Psychic Energy");
-        Deck d = new Deck();
-        Pokemon p1 = (Pokemon) new CardGenerator().generateCard("Charizard");
-        Pokemon p2 = (Pokemon) new CardGenerator().generateCard("Pikachu");
-        Pokemon p3 = (Pokemon) new CardGenerator().generateCard("Squirtle");
-
-        d.addCard(p1);
-        d.addCard(e);
-        d.addCard(p2);
-        d.addCard(trainer);
-        d.addCard(p3);
-        d.addCard(e2);
-
-        assertEquals(6, d.size());
-        assertEquals(p1, d.getCards().get(0));
-        assertEquals(e, d.getCards().get(1));
-        assertEquals(p2, d.getCards().get(2));
-        assertEquals(trainer, d.getCards().get(3));
-        assertEquals(p3, d.getCards().get(4));
-        assertEquals(e2, d.getCards().get(5));
+        try (FileReader reader = new FileReader("base1.json")) {
+            String content = new String(Files.readAllBytes(Paths.get("base1.json")));
+            JSONArray pokemonArray = new JSONArray(content);
+            for (int i = 0; i < pokemonArray.length(); i++) {
+                String PokemonName = pokemonArray.getJSONObject(i).getString("name");
+                Card card = cg.generateCard(PokemonName);
+                allCards.add(card);
+            }
+        } catch (IOException e) {
+            System.out.println("File not found in getAllCards" + e);
+            return null;
+        }
+        return allCards;
     }
 
     @Test
