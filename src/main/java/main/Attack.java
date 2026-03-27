b57896f7b3baf1d7ea0e3b2aa27d929d468d4f33
Sebastian Alejandro <alejansw@rose-hulman.edu>
2026-03-27 10:41:42 -0400

    change repo structure

--- src/main/java/main/Attack.java
+++ src/main/java/main/Attack.java
@@ -8,12 +8,12 @@ public class Attack {
     public String name;
     public List<Energy> costs;
     public int damage;
+
     public Attack(String name, List<Energy> costs, int damage) {
         this.name = name;
         this.costs = costs;
         this.damage = damage;
     }
-
 
     public int getDamage() {
         return damage;
